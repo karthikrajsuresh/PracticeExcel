b1675263ab031e956141d8575f57b3a79264dd6c
Karthik Raj S <karthikrajsuresh007@gmail.com>
2024-06-28 12:31:55 +0530

    Implementing Custom Getter and Setter

--- PracticeExcel/Models/Bike.cs
+++ PracticeExcel/Models/Bike.cs
@@ -2,7 +2,39 @@
 {
     public class Bike
     {
-        public int MANUFACTURER_ID { get; set; }
-        public string BIKE_NAME { get; set; }
+        private int _manufacturerId;
+        private string _bikeName;
+
+        public int MANUFACTURER_ID
+        {
+            get { return _manufacturerId; }
+            set
+            {
+                if (value > 0)
+                {
+                    _manufacturerId = value;
+                }
+                else
+                {
+                    throw new ArgumentException("MANUFACTURER_ID must be a positive integer.");
+                }
+            }
+        }
+
+        public string BIKE_NAME
+        {
+            get { return _bikeName; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _bikeName = value.Trim();
+                }
+                else
+                {
+                    throw new ArgumentException("BIKE_NAME cannot be null or empty.");
+                }
+            }
+        }
     }
 }
--- PracticeExcel/Models/Part.cs
+++ PracticeExcel/Models/Part.cs
@@ -2,7 +2,39 @@
 {
     public class Part
     {
-        public int MANUFACTURER_ID { get; set; }
-        public string PART_NO { get; set; }
+        private int _manufacturerId;
+        private string _partNo;
+
+        public int MANUFACTURER_ID
+        {
+            get { return _manufacturerId; }
+            set
+            {
+                if (value > 0)
+                {
+                    _manufacturerId = value;
+                }
+                else
+                {
+                    throw new ArgumentException("MANUFACTURER_ID must be a positive integer.");
+                }
+            }
+        }
+
+        public string PART_NO
+        {
+            get { return _partNo; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _partNo = value.Trim();
+                }
+                else
+                {
+                    throw new ArgumentException("PART_NO cannot be null or empty.");
+                }
+            }
+        }
     }
 }
